f851c474131ed8afabcb112418b145704b1d7dfc
luisCardenasL <130403281+luisCardenasL@users.noreply.github.com>
2025-02-05 15:30:01 -0400

    Update helpFetch.js

--- src/hooks/helpFetch.js
+++ src/hooks/helpFetch.js
@@ -9,7 +9,7 @@ const helpFetch = () => {
       
     }
     options.credentials = 'include'
-    options.mode = 'no-cors'
+    options.mode = 'cors'
 
     if (options.body) {
       options.body = JSON.stringify(options.body)
